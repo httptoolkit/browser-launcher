42a5c93fd2ee1b5e66ff3a9c95c612cca334e370
Mitchell Hentges <mitchhentges@protonmail.com>
2016-09-05 13:29:21 -0700

    Refresh browser-detect cache if browser not found Fix indentation of index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var path = require( 'path' ),
-	pick = require( 'lodash.pick' ),
-	configModule = require( './lib/config' ),
-	detect = require( './lib/detect' ),
-	run = require( './lib/run' ),
-	createProfiles = require( './lib/create_profiles' );
+var path = require('path'),
+    pick = require('lodash.pick'),
+    configModule = require('./lib/config'),
+    detect = require('./lib/detect'),
+    run = require('./lib/run'),
+    createProfiles = require('./lib/create_profiles');
 
 /**
  * Check the configuration and prepare a launcher function.
@@ -12,72 +12,72 @@ var path = require( 'path' ),
  * @param {String}   [configFile] Path to a configuration file
  * @param {Function} callback     Callback function
  */
-function getLauncher( configFile, callback ) {
-	if ( typeof configFile === 'function' ) {
-		callback = configFile;
-		configFile = configModule.defaultConfigFile;
-	}
+function getLauncher(configFile, callback) {
+    if (typeof configFile === 'function') {
+        callback = configFile;
+        configFile = configModule.defaultConfigFile;
+    }
 
-	configModule.read( configFile, function( err, config ) {
-		if ( !config ) {
-			getLauncher.update( configFile, function( err, config ) {
-				if ( err ) {
-					callback( err );
-				} else {
-					callback( null, wrap( config ) );
-				}
-			} );
-		} else {
-			callback( null, wrap( config ) );
-		}
-	} );
+    configModule.read(configFile, function (err, config) {
+        if (!config) {
+            getLauncher.update(configFile, function (err, config) {
+                if (err) {
+                    callback(err);
+                } else {
+                    callback(null, wrap(config));
+                }
+            });
+        } else {
+            callback(null, wrap(config));
+        }
+    });
 
-	function wrap( config ) {
-		var res = launch.bind( null, config );
+    function wrap(config) {
+        var res = launch.bind(null, config);
 
-		res.browsers = config.browsers;
+        res.browsers = config.browsers;
 
-		return res;
-	}
+        return res;
+    }
 
-	function launch( config, uri, options, callback ) {
-		if ( typeof options === 'string' ) {
-			options = {
-				browser: options
-			};
-		}
+    function launch(config, uri, options, callback) {
+        if (typeof options === 'string') {
+            options = {
+                browser: options
+            };
+        }
 
-		options = options || {};
+        options = options || {};
 
-		var version = options.version || options.browser.split( '/' )[ 1 ] || '*',
-			name = options.browser.toLowerCase().split( '/' )[ 0 ],
-			runner = run( config, name, version );
+        var version = options.version || options.browser.split('/')[1] || '*',
+            name = options.browser.toLowerCase().split('/')[0],
+            runner = run(config, name, version);
 
-		if ( !runner ) {
-			// update the list of available browsers and retry
-			getLauncher.update( configFile, function( err, config ) {
-				if ( !( runner = run( config, name, version ) ) ) {
-					return callback( name + ' is not installed in your system.' );
-				}
+        if (!runner) {
+            // update the list of available browsers and retry
+            getLauncher.update(configFile, function (err, config) {
+                if (!( runner = run(config, name, version) )) {
+                    return callback(name + ' is not installed in your system.');
+                }
 
-				runner( uri, options, callback );
-			} );
-		} else {
-			runner( uri, options, callback );
-		}
-	}
+                runner(uri, options, callback);
+            });
+        } else {
+            runner(uri, options, callback);
+        }
+    }
 }
 
 /**
  * Detect available browsers
  * @param {Function} callback Callback function
  */
-getLauncher.detect = function( callback ) {
-	detect( function( browsers ) {
-		callback( browsers.map( function( browser ) {
-			return pick( browser, [ 'name', 'version', 'type', 'command' ] );
-		} ) );
-	} );
+getLauncher.detect = function (callback) {
+    detect(function (browsers) {
+        callback(browsers.map(function (browser) {
+            return pick(browser, ['name', 'version', 'type', 'command']);
+        }));
+    });
 };
 
 /**
@@ -85,31 +85,31 @@ getLauncher.detect = function( callback ) {
  * @param {String}   configFile Path to the configuration file
  * @param {Function} callback  Callback function
  */
-getLauncher.update = function( configFile, callback ) {
-	if ( typeof configFile === 'function' ) {
-		callback = configFile;
-		configFile = configModule.defaultConfigFile;
-	}
+getLauncher.update = function (configFile, callback) {
+    if (typeof configFile === 'function') {
+        callback = configFile;
+        configFile = configModule.defaultConfigFile;
+    }
 
-	detect( function( browsers ) {
-		createProfiles( browsers, path.dirname( configFile ), function( err ) {
-			if ( err ) {
-				return callback( err );
-			}
+    detect(function (browsers) {
+        createProfiles(browsers, path.dirname(configFile), function (err) {
+            if (err) {
+                return callback(err);
+            }
 
-			var config = {
-				browsers: browsers
-			};
+            var config = {
+                browsers: browsers
+            };
 
-			configModule.write( configFile, config, function( err ) {
-				if ( err ) {
-					callback( err );
-				} else {
-					callback( null, config );
-				}
-			} );
-		} );
-	} );
+            configModule.write(configFile, config, function (err) {
+                if (err) {
+                    callback(err);
+                } else {
+                    callback(null, config);
+                }
+            });
+        });
+    });
 };
 
 module.exports = getLauncher;
